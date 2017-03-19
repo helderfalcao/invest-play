2e83247a7f1510f13e5164273561d49be6d21d1f
Helder Falcão <helderfalcaob@gmail.com>
2017-03-19 19:10:31 -0300

    Adicionada estrutura inicial de página

---
+++ index.js
@@ -0,0 +1,23 @@
+// Load Libraries
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var path = require('path');
+
+app.use(bodyParser.json());
+
+//Config
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Routes
+var clientRoutes = require('./client/routes/index')
+app.use(express.static(path.join(__dirname, '/client')));
+app.use(express.static(path.join(__dirname, '/bower_components')));
+
+app.set('views', path.join(__dirname, '/client/views'));
+app.set('view engine', 'pug');
+app.use('/', clientRoutes);
+
+app.listen(8075);
+console.log("server running...");
