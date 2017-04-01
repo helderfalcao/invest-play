ba153c9239ee056cf449081f14611f10bd23eeae
paulofigures <paulofigures@gmail.com>
2017-04-01 06:33:39 -0300

    Telas de informação.

--- src/intro/info_4.js
+++ src/intro/info_4.js
@@ -3,7 +3,7 @@ import { HttpClient } from 'aurelia-fetch-client';
 import 'fetch';
 
 @inject(HttpClient)
-export class info_3 {    
+export class info_4 {    
     
     constructor(http) {
         this.http = http;
