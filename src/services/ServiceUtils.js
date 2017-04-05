65681432af72f90ab77f31e0b6a89e6223ee856e
Helder Falcão <helderfalcaob@gmail.com>
2017-04-04 21:50:40 -0300

    Alteração URL services

--- src/services/ServiceUtils.js
+++ src/services/ServiceUtils.js
@@ -1,9 +1,9 @@
 export class ServiceUtils {
-    //API_URL = "https://investplaybr.herokuapp.com/v1/";
-    //CLIENT_URL = "https://d2h8zp45tr6ojt.cloudfront.net/";
+    API_URL = "https://investplaybr.herokuapp.com/v1/";
+    CLIENT_URL = "https://d2h8zp45tr6ojt.cloudfront.net/";
 
-    API_URL = "http://localhost:8888/v1/";
-    CLIENT_URL = "http://localhost:9000/";
+    //API_URL = "http://localhost:8888/v1/";
+    //CLIENT_URL = "http://localhost:9000/";
     validResponse = function (response) {
         if (response.status == 200) {
             console.log(response)
