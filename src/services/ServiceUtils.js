e63a1e2d7b6ca47d101a9cd80b85d0dd08e0c99c
Helder Falcão <helderfalcaob@gmail.com>
2017-04-22 10:49:11 -0300

    Configuração ambiente de HOM/DEV

--- src/services/ServiceUtils.js
+++ src/services/ServiceUtils.js
@@ -1,9 +1,15 @@
 export class ServiceUtils {
-    API_URL = "https://investplaybr.herokuapp.com/v1/";
-    CLIENT_URL = "https://d2h8zp45tr6ojt.cloudfront.net/";
+    // PRD Endpoints
+    //API_URL = "https://investplaybr.herokuapp.com/v1/";
+    //CLIENT_URL = "https://d2h8zp45tr6ojt.cloudfront.net/";
 
-    //API_URL = "http://localhost:8888/v1/";
+    //DEV Endpoints
+    //API_URL = "https://investplaybrdev.herokuapp.com/v1/";
     //CLIENT_URL = "http://localhost:9000/";
+
+    //HOM Endpoints
+    API_URL = "https://investplayhom.herokuapp.com/v1/";
+    CLIENT_URL = "https://d3879c8quvq0ty.cloudfront.net/";
     validResponse = function (response) {
         if (response.status == 200) {
             console.log(response)
