47c1789711ec0dfb7e767cff04e744f5b59d1419
paulofigures <paulofigures@gmail.com>
2017-06-02 10:00:24 -0300

    Arquivos da LandingPage

--- src/services/ServiceUtils.js
+++ src/services/ServiceUtils.js
@@ -4,19 +4,19 @@ export class ServiceUtils {
     //CLIENT_URL = "https://d2h8zp45tr6ojt.cloudfront.net/";
 
     //DEV Endpoints
-    //API_URL = "https://investplaybrdev.herokuapp.com/v1/";
-    //CLIENT_URL = "http://localhost:9000/";
+    API_URL = "https://investplaybrdev.herokuapp.com/v1/";
+    CLIENT_URL = "http://localhost:9000/";
 
     //HOM Endpoints
-    API_URL = "https://investplayhom.herokuapp.com/v1/";
-    CLIENT_URL = "https://d3879c8quvq0ty.cloudfront.net/";
-    validResponse = function (response) {
-        if (response.status == 200) {
-            console.log(response)
-            return true;
-        } else {
-            console.log(response);
-            return false;
-        }
-    }
+    // API_URL = "https://investplayhom.herokuapp.com/v1/";
+    // CLIENT_URL = "https://d3879c8quvq0ty.cloudfront.net/";
+    // validResponse = function (response) {
+    //     if (response.status == 200) {
+    //         console.log(response)
+    //         return true;
+    //     } else {
+    //         console.log(response);
+    //         return false;
+    //     }
+    // }
 }
