1d23560d82c38974210f906e69e3e29f61b4e294
Helder Falcão <helderfalcaob@gmail.com>
2017-07-03 23:33:21 -0300

    Configuração fluxo login

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import { inject } from 'aurelia-framework';
 import 'fetch';
 
 export class App {
-  
+
   constructor() {
   }
 
@@ -10,15 +10,7 @@ export class App {
     config.title = 'Investplay';
     config.map([
       { route: ['', 'apresentacao'], name: 'apresentacao', moduleId: 'intro/apresentacao', nav: true },
-      { route: 'login', name: 'login', moduleId: 'intro/login', nav: true },
-      { route: 'investimento', name: 'investimento', moduleId: 'perfil/investimento', nav: true },
-      { route: 'objetivo', name: 'objetivo', moduleId: 'perfil/objetivo', nav: true },
-      { route: 'carteira', name: 'carteira', moduleId: 'perfil/carteira', nav: true },
-      { route: 'modulos', name: 'modulos', moduleId: 'dashboard/modulos', nav: true },
-      { route: 'quiz', name: 'quiz', moduleId: 'dashboard/quiz', nav: true },
-      { route: 'md-select',  name: 'md-select', moduleId: 'material/select/md-select', nav: true, title: 'Select' },
-      { route: 'md-button',  name: 'md-button', moduleId: 'material/button/md-button', nav: true, title: 'Button' },
-      { route: 'md-slider',  name: 'md-slider', moduleId: 'material/slider/md-slider', nav: true, title: 'Slider' }
+      { route: 'login', name: 'login', moduleId: 'intro/login', nav: true }
     ]);
 
     this.router = router;
---
+++ src/intro/login.js
@@ -0,0 +1,12 @@
+import { inject } from 'aurelia-dependency-injection';
+
+export class Apresentacao {
+
+    constructor() {
+    }
+
+    nextScreen() {
+        console.log('teste');
+    }
+
+}
