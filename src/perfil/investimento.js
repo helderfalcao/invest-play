19c8cbb7184b9a20ddbb0e87170a88c41ce12e0d
Helder Falcão <helderfalcaob@gmail.com>
2017-07-16 22:21:49 -0300

    Implementação cenários de investimento

--- src/perfil/investimento.js
+++ src/perfil/investimento.js
@@ -1,15 +1,17 @@
 import { inject } from 'aurelia-dependency-injection';
 import { Router } from 'aurelia-router';
+import { InvestimentoService } from '../services/InvestimentoService';
 
-@inject(Router)
+@inject(Router, InvestimentoService)
 export class Investimento {
-    
-    constructor(router) {
+
+    constructor(router, investimentoService) {
+        this.investimentoService = investimentoService;
         this.router = router;
         this.investRange = this.createRange("Valor Atual", 0, 5000);
         this.mesInvestRange = this.createRange("Valor mensal", 0, 1000);
-        this.riscoRange = this.createRange("Risco(escala de 0 a 10)", 0, 10);;
-        this.risco = 0;
+        this.tempoRange = this.createRange("Tempo(escala de 0 a 10)", 0, 10);;
+        this.tempo = 0;
         this.min = 0;
         this.max = 5000;
     }
@@ -21,10 +23,18 @@ export class Investimento {
     createRange(label, min, max) {
         return {
             "label": label,
-            "min" : min,
-            "max" : max,
-            "value" : 0
+            "min": min,
+            "max": max,
+            "value": 0
         }
+    }
+
+    salvarInvestimento() {
+        this.investimentoService.salvar(
+            this.investRange.value,
+            this.mesInvestRange.value,
+            this.tempoRange.value
+        )
     }
 
 }
---
+++ src/services/InvestimentoService.js
@@ -0,0 +1,31 @@
+import { inject } from 'aurelia-framework';
+import { HttpService } from '../services/HttpService';
+import 'fetch';
+
+@inject(HttpService)
+export class InvestimentoService {
+
+    pergunta;
+
+    constructor(http) {
+        this.http = http;
+    }
+
+    salvar(valorAtual, valorMensal, tempo) {
+        var investimento = {
+            "valorInicial": valorAtual,
+            "valorMensal": valorMensal,
+            "tempoInvestimento": tempo
+        }
+        var request = "investimentos";
+        this.http.POST(request, investimento)
+            .then(response => response.json())
+            .then(data => {
+                console.log("sucesso");
+            }).catch(function (error) {
+                console.log(error);
+            });
+
+    }
+
+}
