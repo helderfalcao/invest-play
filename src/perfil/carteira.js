276540ebbe605a9828932abda5f24e0bc08cf869
Helder Falcão <helderfalcaob@gmail.com>
2017-08-07 00:09:56 -0300

    Consumo serviço, templatização serviço

--- src/perfil/carteira.js
+++ src/perfil/carteira.js
@@ -1,11 +1,40 @@
 import { inject } from 'aurelia-dependency-injection';
 import { Router } from 'aurelia-router';
+import { InvestimentoService } from '../services/InvestimentoService'
 
-@inject(Router)
+@inject(Router, InvestimentoService)
 export class Carteira {
-    
-    constructor(router) {
+
+    investimentos = [
+        {
+            produto: "Fundo Garantidor de Credito",
+            valor: "49.117,75"
+        },
+        {
+            produto: "Tesouro direto",
+            valor: "45.700,70"
+        },
+        {
+            produto: "LCI/LCA",
+            valor: "48.713,39"
+        },
+        {
+            produto: "LC",
+            valor: "48.034,49"
+        },
+        {
+            produto: "Fundo DI",
+            valor: "47.834,49"
+        },
+        {
+            produto: "Poupança",
+            valor: "47.017,49"
+        },
+    ]
+
+    constructor(router, investimentoService) {
         this.router = router;
+        this.investimentoService = investimentoService;
     }
 
     nextScreen() {
--- src/services/InvestimentoService.js
+++ src/services/InvestimentoService.js
@@ -22,8 +22,23 @@ export class InvestimentoService {
     }
 
     buscarInvestimentoAtual() {
-        var request = "investimentoUsuario";
+        var request = "investimentoRendimento";
         return this.http.GET(request);
+    }
+
+    calcularInvestimentoProduto(valorInicial, valorMensal, quantidadeMes, produto, porcentagem) {
+        var investQuery = "?valorInicial={0}}&valorMensal={1}&quantidadeMes={2}&produto={3}";
+        investQuery.format(
+            this.getInvestimentoReal(valorInicial),
+            this.getInvestimentoReal(valorMensal),
+            quantidadeMes,
+            produto,
+            porcentagem);
+        return this.http.GET(investimentoUsuario + investQuery);
+    }
+
+    getInvestimentoReal(investimento, porcentagem) {
+        return investimento * (porcentagem / 100);
     }
 
 }
